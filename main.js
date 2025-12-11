fdf7b50c2e1aec2b4510967c579dabd1658f0a25
seoyooncho1228 <myt4xsp6ys@privaterelay.appleid.com>
2025-12-12 02:58:06 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -44,7 +44,7 @@ document.addEventListener("mousemove", (e) => {
         "graphic01": { category:"graphic", title:"〈Digital Panopticon : 잠들지 않는 눈과 귀〉 Graphic Design", subtitle:"2025 덕성여자대학교 졸업전시회 프로젝트", 
             description:"2025 덕성여자대학교 IT미디어공학전공 졸업전시회 wave to 덕 팀의 프로젝트 〈Digital Panopticon : 잠들지 않는 눈과 귀〉 그래픽 디자인입니다. ",
             mainImage:"/assets/graphics/graphic01_1.png", 
-            detailImages:["assets/graphics/graphic01_1.png"] }
+            detailImages:["assets/graphics/graphic01_2.png"] }
         // 필요한 만큼 추가
     };
 
