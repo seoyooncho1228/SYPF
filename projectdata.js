cb58bbbcaeb7b34cce90d480faa6433bf1929219
seoyooncho1228 <myt4xsp6ys@privaterelay.appleid.com>
2025-12-13 15:25:44 +0900

    Add files via upload

---
+++ projectdata.js
@@ -0,0 +1,57 @@
+
+export const projectData = {
+"branding01": { 
+        category:"branding", 
+        title:"작품 제목 01", 
+        subtitle:"간단 설명", 
+        description:"브랜딩 프로젝트 01 상세 설명입니다.", 
+        mainImage:"assets/branding/branding01_main.png", 
+        detailImages:["assets/branding/branding01_1.png","assets/branding/branding01_2.png"] 
+    },
+
+ "uxui01": { 
+        category:"uxui", 
+        title:"OH MY GUT!", 
+        subtitle:"인터랙티브 AI 수호령 웹 프로젝트", 
+      description: [
+            "Interactive Web",
+            "-",
+        "2025",
+    "Visual Studio Code",
+    "Blender",
+    "-",
+    "3momth"
+],
+        mainImage:"assets/movie/movie01_1.png", 
+        detailImages:["assets/graphics/graphic01_2.png"]     },
+"graphic01": { 
+        category:"graphic", 
+        title:"Digital Panopticon : 잠들지 않는 눈과 귀", 
+        subtitle:"2025 덕성여자대학교 졸업전시회 프로젝트", 
+        description:"2025 덕성여자대학교 IT미디어공학전공 졸업전시회 wave to 덕 팀의 프로젝트 〈Digital Panopticon : 잠들지 않는 눈과 귀〉 그래픽 디자인입니다.", 
+        mainImage:"assets/graphics/graphic01_1.png", 
+        detailImages:["assets/graphics/graphic01_2.png"] 
+        
+    },
+
+ "movie01": { 
+        category:"movie", 
+        title:"Where R GEMINI?", 
+        subtitle:"GOOGLE GEMINI 브랜딩 캠페인 영상 프로젝트", 
+      description: [
+            "3D Cinematic Music Video",
+            "-",
+        "2025",
+    "Blender",
+    "Mid journey",
+    "Runway Gen 4",
+    "-",
+    "1920*1080, 3m 04s",
+    "3momth"
+],
+        mainImage:"assets/movie/movie01_1.png", 
+        video:"https://www.youtube.com/embed/nKtKnn74uv0?si=L_Jyjttkq5Q1wQW9",   // ✅ 추가
+        detailImages:[]     }
+        
+    }
+
